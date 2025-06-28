897cf287b5629c3661045367bdb86c4726c8188d
Mohit Kumar Agarwal <agarwalmohit3010@gmail.com>
2025-06-28 12:53:13 +0530

    Create typed.js

---
+++ typed.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typed-text", {
+            strings: [
+                "React Developer",
+                "Future Full-Stack Engineer",
+                "Tech Explorer",
+                "Web Enthusiast"
+            ],
+            typeSpeed: 50,
+            backSpeed: 30,
+            loop: true,
+});
