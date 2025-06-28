ffb5458a2b02a585bda4343e10f797cb47ab5ba8
Mohit Kumar Agarwal <agarwalmohit3010@gmail.com>
2025-06-28 12:56:21 +0530

    Update typed.js

--- typed.js
+++ typed.js
@@ -1,9 +1,9 @@
 var typed = new Typed("#typed-text", {
             strings: [
-                "React Developer",
-                "Future Full-Stack Engineer",
-                "Tech Explorer",
-                "Web Enthusiast"
+                "Developer",
+                "Full-Stack Engineer",
+                "Designer"
+                
             ],
             typeSpeed: 50,
             backSpeed: 30,
